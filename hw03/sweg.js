0dda4068eb351d12d49cc340c47be05a124c8640
map32 <freepina@gmail.com>
2016-02-25 12:36:35 -0500

    cancer docs removed, hw03 done

--- hw03/sweg.js
+++ hw03/sweg.js
@@ -1,27 +1,27 @@
 var c = document.getElementById("kyk");
 var img = new Image();
-img.src = "";
+img.src = "https://secure.static.tumblr.com/2f66702c53598952d1fa7b828ed61ad6/flsthpo/v3Rnfuv98/tumblr_static_tumblr_static_d9vendggs9kcg84ocssg40okk_640.jpg";
 ctx = c.getContext("2d");
 ctx.fillStyle = "#FF0000";
 var pretty = function(){
-    var dx = 1;
-    var dy = 1;
-    var x = Math.random()*c.width;
-    var y = Math.random()*c.height;
+    var dx = 5;
+    var dy = 5;
+    var x = Math.random()*(c.width-200);
+    var y = Math.random()*(c.height-100);
     var draw = function(){
 	x += dx;
 	y += dy;
 	ctx.clearRect(0,0,c.width,c.height);
     	ctx.drawImage(img, x, y, 200, 100);
-	if (x >= c.width){
-		dx = -1;	
+	if (x+200 >= c.width){
+		dx = -5;	
 	} else if (x <= 0) {
-		dx = 1;
+		dx = 5;
 	}
-	if (y >= c.height){
-		dy = 1;	
+	if (y+100 >= c.height){
+		dy = -5;	
 	} else if (y <= 0) {
-		dy = 1;
+		dy = 5;
 	}
 	framy = requestAnimationFrame(draw);
     }
